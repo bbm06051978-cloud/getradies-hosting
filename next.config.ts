6323c0b65391385794770e57903c8faaddf3142c
Yash Nimbalkar <Yashn@cygen.au>
2026-07-26 16:32:10 +0530

    Fix Prisma datasource URL for Amplify deployment

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import withPWA from "@ducanh2912/next-pwa";
-
+process.env.DATABASE_URL = process.env.DATABASE_URL || "postgresql://postgres.udmlgmmbcuvtpbhdalfb:AJSaanvi1234@aws-1-ap-southeast-2.pooler.supabase.com:6543/postgres?pgbouncer=true";
+process.env.DIRECT_URL = process.env.DIRECT_URL || "postgresql://postgres.udmlgmmbcuvtpbhdalfb:AJSaanvi1234@aws-1-ap-southeast-2.pooler.supabase.com:5432/postgres";
 const nextConfig = withPWA({
   dest: "public",
   cacheOnFrontEndNav: true,
